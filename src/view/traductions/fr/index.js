92b6edec93dd6955357254dc4e7ca34e21cd847a
jessy-bgl <jessy.bogalho@gmail.com>
2023-10-13 07:49:27 +0200

    navigation traductions

--- src/view/traductions/fr/index.js
+++ src/view/traductions/fr/index.js
@@ -1,4 +1,5 @@
 import common from "./common.json";
 import emissions from "./emissions.json";
+import navigation from "./navigation.json";
 
-export { common, emissions };
+export { common, emissions, navigation };
