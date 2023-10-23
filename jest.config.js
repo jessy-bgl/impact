73aed69783bd742fdafc2b922fdddaf95f00e098
jessy-bgl <jessy.bogalho@gmail.com>
2023-10-23 20:10:07 +0200

    code coverage script & config added

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
+  coveragePathIgnorePatterns: ["common"],
 };
