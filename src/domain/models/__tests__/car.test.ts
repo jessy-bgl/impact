eab236e379567064e30bb153593fccdca4d5c300
jessy-bgl <jessy.bogalho@gmail.com>
2023-10-23 19:50:04 +0200

    (wip) car tests

---
+++ src/domain/models/__tests__/car.test.ts
@@ -0,0 +1,15 @@
+import { Car } from "../transport/car/Car";
+
+describe("Car", () => {
+  it("should give an annual footprint of 2231 kgCO2e with default values", () => {
+    const car = new Car({});
+    expect(car.annualFootprint).toBe(2231);
+  });
+
+  it("should give a default annual footprint of 0 kgCO2e if we drive 0 km / year", () => {
+    const car = new Car({ kmPerYear: 0 });
+    expect(car.annualFootprint).toBe(0);
+  });
+
+  // TODO : ajouter d'autres tests
+});
