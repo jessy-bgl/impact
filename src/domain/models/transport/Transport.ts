654d2bbc89103ad19a1aff16aa91d0605d0053ae
jessy-bgl <jessy.bogalho@gmail.com>
2023-10-26 08:57:53 +0200

    twoWheeler model

--- src/domain/models/transport/Transport.ts
+++ src/domain/models/transport/Transport.ts
@@ -1,23 +1,28 @@
 import { Car } from "./car/Car";
+import { TwoWheeler } from "./two-wheeler/TwoWheeler";
 
 // TODO : ajouter les autres categories
 
 export enum TransportCategories {
   CAR = "car",
+  TWO_WHEELER = "two-wheeler",
 }
 
 type Props = {
   car?: Car;
+  twoWheeler?: TwoWheeler;
 };
 
 export class Transport {
   car: Car;
+  twoWheeler: TwoWheeler;
 
-  constructor({ car }: Props) {
+  constructor({ car, twoWheeler }: Props) {
     this.car = new Car(car ?? {});
+    this.twoWheeler = new TwoWheeler(twoWheeler ?? {});
   }
 
   public get annualFootprint(): number {
-    return this.car.annualFootprint;
+    return this.car.annualFootprint + this.twoWheeler.annualFootprint;
   }
 }
