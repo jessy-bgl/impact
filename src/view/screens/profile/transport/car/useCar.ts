a9d8bb3ee420e54695b9e252168d4a7262e03391
jessy-bgl <jessy.bogalho@gmail.com>
2023-10-30 21:49:52 +0100

    car - fix form update

--- src/view/screens/profile/transport/car/useCar.ts
+++ src/view/screens/profile/transport/car/useCar.ts
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 import { UsecasesContext } from "../../../../../common/UsecasesContext";
@@ -13,29 +13,37 @@ export type FormValues = Omit<StringifyProperties<Car>, "annualFootprint">;
 
 export const useCar = () => {
   const storedCar = useAppStore((store) => store.transport.car);
+  const storeRegularUser = useAppStore(
+    (store) => store.transport.car.regularUser,
+  );
   const { useUpdateTransport } = useContext(UsecasesContext);
   const { updateCar } = useUpdateTransport();
 
-  const { control, getValues, setValue, watch } = useForm<FormValues>({
-    defaultValues: {
-      kmPerYear: storedCar.kmPerYear.toString(),
-      regularUser: storedCar.regularUser.toString(),
-      size: storedCar.size.toString(),
-      engine: storedCar.engine.toString(),
-      fuelType: storedCar.fuelType.toString(),
-      age: storedCar.age.toString(),
-      averagePassengers: storedCar.averagePassengers.toString(),
-      averageFuelConsumption: storedCar.averageFuelConsumption.toString(),
-    },
+  const getDefaultValues = () => ({
+    kmPerYear: storedCar.kmPerYear.toString(),
+    regularUser: storedCar.regularUser.toString(),
+    size: storedCar.size.toString(),
+    engine: storedCar.engine.toString(),
+    fuelType: storedCar.fuelType.toString(),
+    age: storedCar.age.toString(),
+    averagePassengers: storedCar.averagePassengers.toString(),
+    averageFuelConsumption: storedCar.averageFuelConsumption.toString(),
+  });
+
+  const { control, getValues, watch, reset } = useForm<FormValues>({
+    defaultValues: getDefaultValues(),
   });
 
   const handleUpdate = (field: keyof FormValues) => {
     const stringValue = getValues(field);
     const value = convertStringToType(stringValue, typeof storedCar[field]);
-    if (stringValue === "") setValue(field, value.toString());
     storedCar[field] = value as never;
     updateCar(storedCar);
   };
+
+  useEffect(() => {
+    if (!storeRegularUser) reset(getDefaultValues());
+  }, [storeRegularUser]);
 
   const regularUser = watch("regularUser") === "true";
 
