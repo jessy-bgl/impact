1f45cde1675dd8af7ea6d33308a90eff6e7b6368
jessy-bgl <jessy.bogalho@gmail.com>
2023-11-02 13:46:59 +0100

    profile - transport : plane section added

--- src/view/screens/profile/transport/plane/PlaneSection.tsx
+++ src/view/screens/profile/transport/plane/PlaneSection.tsx
@@ -1,17 +1,125 @@
+import { Controller } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { List } from "react-native-paper";
+import { List, SegmentedButtons, Text, TextInput } from "react-native-paper";
 
+import { FormValues, usePlane } from "./usePlane";
 import { ListContentContainer } from "../../components/ListContentContainer";
+import { ListItemDivider } from "../../components/ListItemDivider";
+import { ListTitle } from "../../components/ListTitle";
+import { RowContainer } from "../../components/RowContainer";
 
 export const PlaneSection = () => {
   const { t } = useTranslation(["transport", "emissions", "common"]);
+  const { control, handleUpdate, usage, annualFootprint } = usePlane();
 
   return (
     <List.Accordion
-      title={t("emissions:transport.plane")}
+      title={
+        <ListTitle
+          title={t("emissions:transport.plane")}
+          subtitle={`${annualFootprint} ${t("common:footprintKgPerYear")}`}
+        />
+      }
       left={(props) => <List.Icon {...props} icon="airplane" />}
     >
-      <ListContentContainer />
+      <ListContentContainer>
+        <RowContainer>
+          <Text variant="labelLarge">{t("plane.usage")}</Text>
+          <Controller<FormValues>
+            name="usage"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <SegmentedButtons
+                density="small"
+                value={value}
+                onValueChange={(e) => {
+                  onChange(e);
+                  handleUpdate("usage");
+                }}
+                buttons={[
+                  { value: "true", label: t("common:yes") },
+                  { value: "false", label: t("common:no") },
+                ]}
+              />
+            )}
+          />
+        </RowContainer>
+
+        <ListItemDivider />
+
+        <RowContainer>
+          <Text variant="labelLarge" style={{ flex: 2 }}>
+            {t("plane.hoursPerYearInShortHaul")}
+          </Text>
+          <Controller<FormValues>
+            name="hoursPerYearInShortHaul"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                dense
+                mode="outlined"
+                keyboardType="numeric"
+                right={<TextInput.Affix text="h" />}
+                style={{ flex: 1 }}
+                onBlur={() => handleUpdate("hoursPerYearInShortHaul")}
+                onChangeText={onChange}
+                value={value}
+                disabled={!usage}
+              />
+            )}
+          />
+        </RowContainer>
+
+        <ListItemDivider />
+
+        <RowContainer>
+          <Text variant="labelLarge" style={{ flex: 2 }}>
+            {t("plane.hoursPerYearInMediumHaul")}
+          </Text>
+          <Controller<FormValues>
+            name="hoursPerYearInMediumHaul"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                dense
+                mode="outlined"
+                keyboardType="numeric"
+                right={<TextInput.Affix text="h" />}
+                style={{ flex: 1 }}
+                onBlur={() => handleUpdate("hoursPerYearInMediumHaul")}
+                onChangeText={onChange}
+                value={value}
+                disabled={!usage}
+              />
+            )}
+          />
+        </RowContainer>
+
+        <ListItemDivider />
+
+        <RowContainer>
+          <Text variant="labelLarge" style={{ flex: 2 }}>
+            {t("plane.hoursPerYearInLongHaul")}
+          </Text>
+          <Controller<FormValues>
+            name="hoursPerYearInLongHaul"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                dense
+                mode="outlined"
+                keyboardType="numeric"
+                right={<TextInput.Affix text="h" />}
+                style={{ flex: 1 }}
+                onBlur={() => handleUpdate("hoursPerYearInLongHaul")}
+                onChangeText={onChange}
+                value={value}
+                disabled={!usage}
+              />
+            )}
+          />
+        </RowContainer>
+      </ListContentContainer>
     </List.Accordion>
   );
 };
---
+++ src/view/screens/profile/transport/plane/usePlane.ts
@@ -0,0 +1,40 @@
+import { useContext, useEffect } from "react";
+import { DefaultValues } from "react-hook-form";
+
+import { UsecasesContext } from "../../../../../common/UsecasesContext";
+import { useAppStore } from "../../../../../data/store/store";
+import { Plane } from "../../../../../domain/models/transport/plane/Plane";
+import { StringifyProperties } from "../../../../../types/utils";
+import { useUpdateForm } from "../../utils/useUpdateForm";
+
+export type FormValues = Omit<StringifyProperties<Plane>, "annualFootprint">;
+
+export const usePlane = () => {
+  const storedPlane = useAppStore((store) => store.transport.plane);
+  const storedPlaneUsage = storedPlane.usage;
+  const annualFootprint = new Plane(storedPlane).annualFootprint;
+
+  const { useUpdateTransport } = useContext(UsecasesContext);
+  const { updatePlane } = useUpdateTransport();
+
+  const getDefaultValues = (): DefaultValues<FormValues> => ({
+    usage: storedPlane.usage.toString(),
+    hoursPerYearInShortHaul: storedPlane.hoursPerYearInShortHaul.toString(),
+    hoursPerYearInMediumHaul: storedPlane.hoursPerYearInMediumHaul.toString(),
+    hoursPerYearInLongHaul: storedPlane.hoursPerYearInLongHaul.toString(),
+  });
+
+  const { handleUpdate, control, watch, reset } = useUpdateForm<Plane>(
+    getDefaultValues(),
+    storedPlane,
+    updatePlane,
+  );
+
+  useEffect(() => {
+    if (!storedPlaneUsage) reset(getDefaultValues());
+  }, [storedPlaneUsage]);
+
+  const usage = watch("usage") === "true";
+
+  return { control, handleUpdate, usage, annualFootprint };
+};
