03d4e918363e009b68e19e9a2fdbeed349a86f34
jessy-bgl <jessy.bogalho@gmail.com>
2023-11-02 08:25:06 +0100

    plane added to Transport domain model

--- src/domain/models/transport/Transport.ts
+++ src/domain/models/transport/Transport.ts
@@ -1,4 +1,5 @@
 import { Car } from "./car/Car";
+import { Plane } from "./plane/Plane";
 import { TwoWheeler } from "./two-wheeler/TwoWheeler";
 
 // TODO : ajouter les autres categories
@@ -6,23 +7,31 @@ import { TwoWheeler } from "./two-wheeler/TwoWheeler";
 export enum TransportCategories {
   CAR = "car",
   TWO_WHEELER = "two-wheeler",
+  PLANE = "plane",
 }
 
 type Props = {
   car?: Car;
   twoWheeler?: TwoWheeler;
+  plane?: Plane;
 };
 
 export class Transport {
   car: Car;
   twoWheeler: TwoWheeler;
+  plane: Plane;
 
-  constructor({ car, twoWheeler }: Props) {
+  constructor({ car, twoWheeler, plane }: Props) {
     this.car = new Car(car ?? {});
     this.twoWheeler = new TwoWheeler(twoWheeler ?? {});
+    this.plane = new Plane(plane ?? {});
   }
 
   public get annualFootprint(): number {
-    return this.car.annualFootprint + this.twoWheeler.annualFootprint;
+    return Math.round(
+      this.car.annualFootprint +
+        this.twoWheeler.annualFootprint +
+        this.plane.annualFootprint,
+    );
   }
 }
