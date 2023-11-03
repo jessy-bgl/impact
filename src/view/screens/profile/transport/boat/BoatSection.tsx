a17e52bd3778fe962ca221b6661863395e12495a
jessy-bgl <jessy.bogalho@gmail.com>
2023-11-03 08:18:37 +0100

    profile - transport : boat section added

--- src/view/screens/profile/transport/boat/BoatSection.tsx
+++ src/view/screens/profile/transport/boat/BoatSection.tsx
@@ -1,17 +1,75 @@
+import { Controller } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { List } from "react-native-paper";
+import { List, SegmentedButtons, Text, TextInput } from "react-native-paper";
 
+import { FormValues, useBoat } from "./useBoat";
 import { ListContentContainer } from "../../components/ListContentContainer";
+import { ListItemDivider } from "../../components/ListItemDivider";
+import { ListTitle } from "../../components/ListTitle";
+import { RowContainer } from "../../components/RowContainer";
 
 export const BoatSection = () => {
   const { t } = useTranslation(["transport", "emissions", "common"]);
+  const { control, handleUpdate, usage, annualFootprint } = useBoat();
 
   return (
     <List.Accordion
-      title={t("emissions:transport.boat")}
+      title={
+        <ListTitle
+          title={t("emissions:transport.boat")}
+          subtitle={`${annualFootprint} ${t("common:footprintKgPerYear")}`}
+        />
+      }
       left={(props) => <List.Icon {...props} icon="ferry" />}
     >
-      <ListContentContainer />
+      <ListContentContainer>
+        <RowContainer>
+          <Text variant="labelLarge">{t("boat.usage")}</Text>
+          <Controller<FormValues>
+            name="usage"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <SegmentedButtons
+                density="small"
+                value={value}
+                onValueChange={(e) => {
+                  onChange(e);
+                  handleUpdate("usage");
+                }}
+                buttons={[
+                  { value: "true", label: t("common:yes") },
+                  { value: "false", label: t("common:no") },
+                ]}
+              />
+            )}
+          />
+        </RowContainer>
+
+        <ListItemDivider />
+
+        <RowContainer>
+          <Text variant="labelLarge" style={{ flex: 2 }}>
+            {t("boat.hoursPerYear")}
+          </Text>
+          <Controller<FormValues>
+            name="hoursPerYear"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                dense
+                mode="outlined"
+                keyboardType="numeric"
+                right={<TextInput.Affix text="h" />}
+                style={{ flex: 1 }}
+                onBlur={() => handleUpdate("hoursPerYear")}
+                onChangeText={onChange}
+                value={value}
+                disabled={!usage}
+              />
+            )}
+          />
+        </RowContainer>
+      </ListContentContainer>
     </List.Accordion>
   );
 };
---
+++ src/view/screens/profile/transport/boat/useBoat.ts
@@ -0,0 +1,38 @@
+import { useContext, useEffect } from "react";
+import { DefaultValues } from "react-hook-form";
+
+import { UsecasesContext } from "../../../../../common/UsecasesContext";
+import { useAppStore } from "../../../../../data/store/store";
+import { Boat } from "../../../../../domain/models/transport/boat/Boat";
+import { StringifyProperties } from "../../../../../types/utils";
+import { useUpdateForm } from "../../utils/useUpdateForm";
+
+export type FormValues = Omit<StringifyProperties<Boat>, "annualFootprint">;
+
+export const useBoat = () => {
+  const storedBoat = useAppStore((store) => store.transport.boat);
+  const storedBoatUsage = storedBoat.usage;
+  const annualFootprint = new Boat(storedBoat).annualFootprint;
+
+  const { useUpdateTransport } = useContext(UsecasesContext);
+  const { updateBoat } = useUpdateTransport();
+
+  const getDefaultValues = (): DefaultValues<FormValues> => ({
+    usage: storedBoat.usage.toString(),
+    hoursPerYear: storedBoat.hoursPerYear.toString(),
+  });
+
+  const { handleUpdate, control, watch, reset } = useUpdateForm<Boat>(
+    getDefaultValues(),
+    storedBoat,
+    updateBoat,
+  );
+
+  useEffect(() => {
+    if (!storedBoatUsage) reset(getDefaultValues());
+  }, [storedBoatUsage]);
+
+  const usage = watch("usage") === "true";
+
+  return { control, handleUpdate, usage, annualFootprint };
+};
