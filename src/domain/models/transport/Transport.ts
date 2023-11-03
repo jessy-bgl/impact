ed2d519061a028fffae02f2e5bdfdabb29a3b80f
jessy-bgl <jessy.bogalho@gmail.com>
2023-11-03 08:07:38 +0100

    boat added to Transport domain model

--- src/domain/models/transport/Transport.ts
+++ src/domain/models/transport/Transport.ts
@@ -1,3 +1,4 @@
+import { Boat } from "./boat/Boat";
 import { Car } from "./car/Car";
 import { Plane } from "./plane/Plane";
 import { TwoWheeler } from "./two-wheeler/TwoWheeler";
@@ -14,24 +15,28 @@ type Props = {
   car?: Car;
   twoWheeler?: TwoWheeler;
   plane?: Plane;
+  boat?: Boat;
 };
 
 export class Transport {
   car: Car;
   twoWheeler: TwoWheeler;
   plane: Plane;
+  boat: Boat;
 
-  constructor({ car, twoWheeler, plane }: Props) {
+  constructor({ car, twoWheeler, plane, boat }: Props) {
     this.car = new Car(car ?? {});
     this.twoWheeler = new TwoWheeler(twoWheeler ?? {});
     this.plane = new Plane(plane ?? {});
+    this.boat = new Boat(boat ?? {});
   }
 
   public get annualFootprint(): number {
     return Math.round(
       this.car.annualFootprint +
         this.twoWheeler.annualFootprint +
-        this.plane.annualFootprint,
+        this.plane.annualFootprint +
+        this.boat.annualFootprint,
     );
   }
 }
