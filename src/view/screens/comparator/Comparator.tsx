8cef8d4fd88e596add2343875a4963ed6cce9bbc
jessy-bgl <jessy.bogalho@gmail.com>
2023-11-07 08:01:43 +0100

    minor refactoring

--- src/view/screens/comparator/Comparator.tsx
+++ src/view/screens/comparator/Comparator.tsx
@@ -3,10 +3,5 @@ import { Platform } from "react-native";
 import { ComparatorForMobile } from "./ComparatorForMobile";
 import { ComparatorForWeb } from "./ComparatorForWeb";
 
-export const Comparator = () => {
-  if (Platform.OS === "web") {
-    return <ComparatorForWeb />;
-  } else {
-    return <ComparatorForMobile />;
-  }
-};
+export const Comparator = () =>
+  Platform.OS === "web" ? <ComparatorForWeb /> : <ComparatorForMobile />;
