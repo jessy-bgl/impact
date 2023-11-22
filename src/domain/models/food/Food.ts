45ad5abd301eecde9d96301f384de52f5f7df987
jessy-bgl <jessy.bogalho@gmail.com>
2023-11-22 08:44:54 +0100

    Food domain model updated

--- src/domain/models/food/Food.ts
+++ src/domain/models/food/Food.ts
@@ -1,19 +1,30 @@
+import { Drinks } from "./drinks/Drinks";
 import { Meals } from "./meals/Meals";
+import { Waste } from "./waste/Waste";
 import { WithAnnualFootprint } from "../transport/types";
 
 type Props = {
   meals?: Meals;
+  drinks?: Drinks;
+  waste?: Waste;
 };
 
-// TODO
 export class Food implements WithAnnualFootprint {
   meals: Meals;
+  drinks: Drinks;
+  waste: Waste;
 
-  constructor({ meals }: Props) {
+  constructor({ meals, drinks, waste }: Props) {
     this.meals = new Meals(meals ?? {});
+    this.drinks = new Drinks(drinks ?? {});
+    this.waste = new Waste(waste ?? {});
   }
 
   public get annualFootprint(): number {
-    return Math.round(this.meals.annualFootprint);
+    return Math.round(
+      this.meals.annualFootprint +
+        this.drinks.annualFootprint +
+        this.waste.annualFootprint,
+    );
   }
 }
