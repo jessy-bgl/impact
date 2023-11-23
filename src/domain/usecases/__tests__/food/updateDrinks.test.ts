b9e592eaf97a8fc7b4cbd569a2ccc6ff96bf2776
jessy-bgl <jessy.bogalho@gmail.com>
2023-11-23 08:55:23 +0100

    updateFood usecase - updateDrinks test

---
+++ src/domain/usecases/__tests__/food/updateDrinks.test.ts
@@ -0,0 +1,61 @@
+import { initFakeRepositories } from "../../../../common/UsecasesContext";
+import { Food } from "../../../models/food/Food";
+import { Drinks } from "../../../models/food/drinks/Drinks";
+import { defaultHotDrinksPerWeek } from "../../../models/food/drinks/constants";
+import { createUseUpdateFood } from "../../updateFood";
+
+describe("updateDrinks", () => {
+  let repositories: ReturnType<typeof initFakeRepositories>;
+  let fetchFoodSpy: jest.SpyInstance;
+  let updateFoodSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    repositories = initFakeRepositories();
+    fetchFoodSpy = jest.spyOn(repositories.emissionsRepository, "fetchFood");
+    updateFoodSpy = jest.spyOn(repositories.emissionsRepository, "updateFood");
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should call emissionsRepository.fetchFood and emissionsRepository.updateFood", () => {
+    // Arrange
+    const { updateDrinks } = createUseUpdateFood(
+      repositories.emissionsRepository,
+    )();
+
+    // Act
+    updateDrinks({} as Drinks);
+
+    // Assert
+    expect(fetchFoodSpy).toHaveBeenCalledTimes(1);
+    expect(updateFoodSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should update food with given drinks data", () => {
+    // Arrange
+    const fakeFood = new Food({});
+    repositories.emissionsRepository.injectFakeFood(fakeFood);
+
+    const fakeDrinks = new Drinks({
+      alcoholLitersPerWeek: 2,
+      bottledWater: true,
+      sodaLitersPerWeek: 2,
+      hotDrinksPerWeek: defaultHotDrinksPerWeek,
+    });
+
+    const { updateDrinks } = createUseUpdateFood(
+      repositories.emissionsRepository,
+    )();
+
+    // Act
+    updateDrinks(fakeDrinks);
+
+    // Assert
+    expect(updateFoodSpy).toHaveBeenCalledWith({
+      ...fakeFood,
+      drinks: fakeDrinks,
+    });
+  });
+});
