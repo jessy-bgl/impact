2c2af151737b3958434c81890c7bff5945bb273b
jessy-bgl <jessy.bogalho@gmail.com>
2023-11-29 12:47:53 +0100

    utils method convertStringToType improved

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -12,6 +12,8 @@ export const convertStringToType = (
     return Number(value);
   } else if (targetType === "boolean") {
     return value === "true";
+  } else if (targetType === "object") {
+    return JSON.parse(value);
   }
 
   throw new Error("Unsupported targetType");
