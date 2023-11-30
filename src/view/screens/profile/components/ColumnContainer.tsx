4c774b5e31780501d33acea474486802e4c6365e
jessy-bgl <jessy.bogalho@gmail.com>
2023-11-30 08:10:32 +0100

    component ColumnContainer improved : optional style props added

--- src/view/screens/profile/components/ColumnContainer.tsx
+++ src/view/screens/profile/components/ColumnContainer.tsx
@@ -1,9 +1,12 @@
 import { PropsWithChildren } from "react";
-import { View } from "react-native";
+import { View, ViewStyle } from "react-native";
 
 import { styles } from "./styles";
 
-export const ColumnContainer = ({ children }: PropsWithChildren) => {
+export const ColumnContainer = ({
+  children,
+  style,
+}: PropsWithChildren & { style?: ViewStyle }) => {
   const { columnContainer } = styles;
-  return <View style={columnContainer}>{children}</View>;
+  return <View style={{ ...columnContainer, ...style }}>{children}</View>;
 };
