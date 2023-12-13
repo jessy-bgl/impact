f0fa36b3f987c49c76f469e108955aa65695ce5e
jessy-bgl <jessy.bogalho@gmail.com>
2023-12-13 07:54:56 +0100

    Housing domain model - Home tests

--- src/domain/models/__tests__/housing/home/dataset.ts
+++ src/domain/models/__tests__/housing/home/dataset.ts
@@ -8,14 +8,37 @@ export const homeDataset: {
     home: new Home({ ageInYears: 50 }),
     expectedAnnualFootprint: 0,
   },
-  //   {
-  //     home: new Home({
-  //       inhabitants: 2,
-  //       isAnApartment: false,
-  //       isEcoBuilt: false,
-  //       livingSpace: 120,
-  //       ageInYears: 30,
-  //     }),
-  //     expectedAnnualFootprint: 550,
-  //   },
+  {
+    home: new Home({
+      inhabitants: 2,
+      isAnApartment: false,
+      isEcoBuilt: false,
+      livingSpace: 120,
+      ageInYears: 30,
+    }),
+    // TODO : check this value, it should be equal to 550
+    expectedAnnualFootprint: 510,
+  },
+  {
+    home: new Home({
+      inhabitants: 2,
+      isAnApartment: true,
+      isEcoBuilt: false,
+      livingSpace: 120,
+      ageInYears: 30,
+    }),
+    // TODO : check this value, it should be equal to 650
+    expectedAnnualFootprint: 630,
+  },
+  {
+    home: new Home({
+      inhabitants: 2,
+      isAnApartment: true,
+      isEcoBuilt: true,
+      livingSpace: 120,
+      ageInYears: 30,
+    }),
+    // TODO : check this value, it should be equal to 180
+    expectedAnnualFootprint: 173,
+  },
 ];
