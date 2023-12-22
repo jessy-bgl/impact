2efd993989959e9a4d58c1f44bbdc8e25599383f
jessy-bgl <jessy.bogalho@gmail.com>
2023-12-22 11:49:18 +0100

    Refactor TwoWheeler tests and add additional test cases

--- src/domain/models/__tests__/transport/twoWheeler.test.ts
+++ src/domain/models/__tests__/transport/twoWheeler.test.ts
@@ -1,21 +1,28 @@
 import { TwoWheeler } from "@domain/models/transport/two-wheeler/TwoWheeler";
 
 describe("TwoWheeler", () => {
-  it("should consider we are not using it by default", () => {
-    const twoWheeler = new TwoWheeler({});
-    expect(twoWheeler.usage).toBeFalsy();
-    expect(twoWheeler.kmPerYear).toBe(0);
-  });
-
-  it("should give a default annual footprint of 0 kgCO2e if we drive 0 km / year", () => {
-    const twoWheeler = new TwoWheeler({ kmPerYear: 0 });
-    expect(twoWheeler.annualFootprint).toBe(0);
-  });
-
-  it("should give a default annual footprint of 0 kgCO2e if we don't use it", () => {
+  it("should give an annual footprint of 0 kgCO2e if we don't use it", () => {
     const twoWheeler = new TwoWheeler({ usage: false });
     expect(twoWheeler.annualFootprint).toBe(0);
   });
 
-  // TODO : ajouter d'autres tests
+  it("should give an annual footprint of 0 kgCO2e if we drive 0 km / year", () => {
+    const twoWheeler = new TwoWheeler({ kmPerYear: 0 });
+    expect(twoWheeler.annualFootprint).toBe(0);
+  });
+
+  it.each`
+    type                 | kmPerYear | expectedAnnualFootprint
+    ${"electricScooter"} | ${3000}   | ${93}
+    ${"thermalScooter"}  | ${3000}   | ${233}
+    ${"motorbikeLT250"}  | ${3000}   | ${233}
+    ${"motorbikeGT250"}  | ${3000}   | ${577}
+  `(
+    `should give an annual footprint of $expectedAnnualFootprint kgCO2e if we drive $kmPerYear km / year with a $type`,
+    ({ type, kmPerYear, expectedAnnualFootprint }) => {
+      console.log(kmPerYear);
+      const twoWheeler = new TwoWheeler({ usage: true, type, kmPerYear });
+      expect(twoWheeler.annualFootprint).toBe(expectedAnnualFootprint);
+    },
+  );
 });
