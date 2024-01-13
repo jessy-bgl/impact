7ee818f2c396cfd40d833d2a078fd49e81172b35
jessy-bgl <jessy.bogalho@gmail.com>
2024-01-13 13:13:23 +0100

    iFrameSizer warning ignored

--- logger.config.ts
+++ logger.config.ts
@@ -1,6 +1,12 @@
 const originalWarn = console.warn;
 
 console.warn = (message, ...optionalParams) => {
-  if (message.includes("deprecated") || message.includes("Plausible")) return;
+  if (
+    message.includes("deprecated") ||
+    message.includes("Plausible") ||
+    message.includes("iFrameSizer")
+  )
+    return;
+
   originalWarn(message, ...optionalParams);
 };
