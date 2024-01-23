fd554f9cfb0f6344277b085ee540f398f53b6219
jessy-bgl <jessy.bogalho@gmail.com>
2024-01-23 09:00:00 +0100

    minor refactoring

---
+++ src/view/translations/fr.js
@@ -0,0 +1,19 @@
+import common from "./fr/common.json";
+import pages from "./fr/pages.json";
+import emissions from "./fr/emissions.json";
+import food from "./fr/food.json";
+import transport from "./fr/transport.json";
+import housing from "./fr/housing.json";
+import everydayThings from "./fr/everydayThings.json";
+import publicServices from "./fr/publicServices.json";
+
+export {
+  common,
+  pages,
+  emissions,
+  transport,
+  food,
+  housing,
+  everydayThings,
+  publicServices,
+};
