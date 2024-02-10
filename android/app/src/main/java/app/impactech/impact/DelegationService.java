bb0c16bfa236a61c70bf7f0064f75fd658aa1cd4
jessy-bgl <jessy.bogalho@gmail.com>
2024-02-10 10:44:23 +0100

    Add Android project files and resources

---
+++ android/app/src/main/java/app/impactech/impact/DelegationService.java
@@ -0,0 +1,14 @@
+package app.impactech.impact;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
