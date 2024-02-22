04346732c6033ea67719047cc8615967dc63e5b7
jessy-bgl <jessy.bogalho@gmail.com>
2024-02-22 08:51:44 +0100

    refactor: actions in-memory repository improved

--- src/data/repositories/actions.memory.repository.ts
+++ src/data/repositories/actions.memory.repository.ts
@@ -2,17 +2,13 @@ import { Action } from "@domain/entities/actions/Action";
 import { ActionsRepository } from "@domain/repositories/actions.repository";
 
 export class ActionsInMemoryRepository implements ActionsRepository {
-  private actions: Action[] = [];
+  actions: Action[] = [];
 
   fetchActions(): Action[] {
     return this.actions;
   }
 
   updateActions(actions: Action[]): void {
-    this.actions = actions;
-  }
-
-  injectFakeActions(actions: Action[]): void {
     this.actions = actions;
   }
 }
