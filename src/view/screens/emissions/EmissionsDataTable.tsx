5e010e973487aa0f924c2d518213fb776a8e4336
jessy-bgl <jessy.bogalho@gmail.com>
2024-04-27 13:06:45 +0200

    ui(emissions): table design improved for small screens

--- src/view/screens/emissions/EmissionsDataTable.tsx
+++ src/view/screens/emissions/EmissionsDataTable.tsx
@@ -17,37 +17,37 @@ export const EmissionsDataTable = ({ footprints }: Props) => {
       <DataTable.Header>
         <DataTable.Title>{t("category")}</DataTable.Title>
         <DataTable.Title numeric>{t("annualFootprint")}</DataTable.Title>
-        <DataTable.Title numeric>{t("part")}</DataTable.Title>
       </DataTable.Header>
 
       {Object.values(footprints)
         .sort((a, b) => b.footprint - a.footprint)
         .map((emissionsCategory) => (
           <DataTable.Row key={emissionsCategory.category}>
-            <DataTable.Cell style={{ alignItems: "center" }}>
-              {t(`categories.${emissionsCategory.category}`)}
+            <DataTable.Cell>
+              <View
+                style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+              >
+                <View
+                  style={{
+                    width: 30,
+                    height: 30,
+                    borderRadius: 30 / 2,
+                    backgroundColor: emissionsCategory.color,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text style={{ color: colors.background, fontSize: 12 }}>
+                    {emissionsCategory.part}%
+                  </Text>
+                </View>
+                <Text>{t(`categories.${emissionsCategory.category}`)}</Text>
+              </View>
             </DataTable.Cell>
 
             <DataTable.Cell numeric>
               {emissionsCategory.footprint} {t("common:footprintKg")}
-            </DataTable.Cell>
-
-            <DataTable.Cell numeric>
-              <View
-                style={{
-                  width: 30,
-                  height: 30,
-                  borderRadius: 30 / 2,
-                  backgroundColor: emissionsCategory.color,
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Text style={{ color: colors.background, fontSize: 12 }}>
-                  {emissionsCategory.part}%
-                </Text>
-              </View>
             </DataTable.Cell>
           </DataTable.Row>
         ))}
