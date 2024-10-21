d499052d7882b81052ab57d88d6094f4bbfe71fe
jessy-bgl <jessy.bogalho@gmail.com>
2024-10-21 08:24:45 +0200

    chore: improve AdemeAction types

--- src/domain/entities/action/AdemeAction.ts
+++ src/domain/entities/action/AdemeAction.ts
@@ -1,14 +1,17 @@
+import { DottedName } from "@incubateur-ademe/nosgestesclimat";
+import { EvaluatedNode } from "publicodes";
+
+import { NGCRuleNode } from "@data/ademe-footprint-model";
 import { Action } from "@domain/entities/action/Action";
 import { AdemeEngine } from "@domain/entities/AdemeEngine";
-import { NGCRule } from "@incubateur-ademe/nosgestesclimat";
 
 export class AdemeAction extends Action {
-  constructor(rule: NGCRule) {
+  constructor(rule: NGCRuleNode & EvaluatedNode) {
     super({
       id: rule.dottedName!,
       label: (rule.title as string | undefined) ?? "",
       description: (rule.rawNode as any)?.description ?? "",
-      category: AdemeEngine.getCategory(rule.dottedName!),
+      category: AdemeEngine.getCategory(rule.dottedName as DottedName),
       savedFootprint: rule.nodeValue ? Math.round(rule.nodeValue as number) : 0,
     });
   }
