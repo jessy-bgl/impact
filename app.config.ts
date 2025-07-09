2634ab28d0a8ba816d1e5a7298219d1f64262195
jessy <j.bogalho@m2mfactory.fr>
2025-07-09 08:13:51 +0200

    chore(config): update expo app configuration for better version management

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,20 @@
 import { ExpoConfig } from "expo/config";
+import { coerce } from "semver";
 import "ts-node/register";
 
+import pkg from "./package.json";
+
+const pkgVersion = coerce(pkg.version)!;
+
 const config: ExpoConfig = {
+  owner: "jessy-bgl",
   githubUrl: "https://github.com/jessy-bgl/impact",
   name: "Impact",
   slug: "impact",
-  version: "1.1.1",
+  version: pkgVersion.version,
+  extra: {
+    eas: { projectId: "e2d24387-b1d5-4c95-b5a8-42f48c62af22" },
+  },
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "dark",
@@ -20,22 +29,26 @@ const config: ExpoConfig = {
     ],
   ],
   assetBundlePatterns: ["**/*"],
-  ios: {
-    supportsTablet: true,
-    bundleIdentifier: "com.impactech.impact",
+  experiments: {
+    tsconfigPaths: true,
+    baseUrl: "/impact",
+  },
+  web: {
+    bundler: "metro",
+    favicon: "./assets/favicon.png",
   },
   android: {
+    package: "com.impactech.impact",
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#ffffff",
     },
-    package: "com.impactech.impact",
     edgeToEdgeEnabled: true,
   },
-  web: { bundler: "metro", favicon: "./assets/favicon.png" },
-  experiments: { tsconfigPaths: true, baseUrl: "/impact" },
-  extra: { eas: { projectId: "e2d24387-b1d5-4c95-b5a8-42f48c62af22" } },
-  owner: "jessy-bgl",
+  ios: {
+    bundleIdentifier: "com.impactech.impact",
+    supportsTablet: true,
+  },
 };
 
 export default config;
