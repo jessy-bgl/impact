460e8c2a4dfba27aa5afcad69ca20cd45edfcf39
jessy <j.bogalho@m2mfactory.fr>
2025-08-02 15:45:41 +0200

    chore(eslint): add Jest plugin configuration for test files

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const pluginJest = require("eslint-plugin-jest");
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   { ignores: ["node_modules/", "web-build/", "dist/", "assets/"] },
+  {
+    files: ["**/*.spec.ts", "**/*.test.ts", "**/*.spec.tsx", "**/*.test.tsx"],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+  },
 ]);
