fbc870234196c8d34afa500516882d624fb6b8fe
jessy <j.bogalho@m2mfactory.fr>
2025-08-08 08:23:58 +0200

    fix: jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,6 @@ import { getAliasesInTsConfig } from "./utils";
 export default async (): Promise<Config> => {
   const jestConfig: Config = {
     preset: "jest-expo",
-    setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
     testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
     transformIgnorePatterns: [
       "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
