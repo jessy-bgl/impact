6e9bbf2d539b2122dd362d6dc6d575e04bfbc140
jessy <j.bogalho@m2mfactory.fr>
2025-08-19 08:05:14 +0200

    feat(emissions): add profile completion status in emissions data table

--- src/view/screens/emissions/EmissionsDataTable.tsx
+++ src/view/screens/emissions/EmissionsDataTable.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import { View } from "react-native";
 import { DataTable, Text, useTheme } from "react-native-paper";
 
+import { useAppStore } from "@data/store/store";
 import { Footprints } from "@view/view-models/Footprint";
 
 type Props = {
@@ -13,6 +14,8 @@ export const EmissionsDataTable = ({ footprints }: Props) => {
 
   const { colors } = useTheme();
 
+  const profileCompletion = useAppStore((state) => state.profile.completion);
+
   return (
     <DataTable>
       <DataTable.Header>
@@ -22,35 +25,54 @@ export const EmissionsDataTable = ({ footprints }: Props) => {
 
       {Object.values(footprints)
         .sort((a, b) => b.footprint - a.footprint)
-        .map((emissionsCategory) => (
-          <DataTable.Row key={emissionsCategory.category}>
-            <DataTable.Cell>
-              <View
-                style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
-              >
+        .map((emissionsCategory) => {
+          const isCompleted = Object.values(
+            profileCompletion[emissionsCategory.category],
+          ).every(Boolean);
+          return (
+            <DataTable.Row key={emissionsCategory.category}>
+              <DataTable.Cell>
                 <View
                   style={{
-                    width: 30,
-                    height: 30,
-                    borderRadius: 30 / 2,
-                    backgroundColor: emissionsCategory.color,
                     flexDirection: "row",
                     alignItems: "center",
-                    justifyContent: "center",
+                    gap: 10,
                   }}
                 >
-                  <Text style={{ color: colors.background, fontSize: 12 }}>
-                    {emissionsCategory.part}%
-                  </Text>
+                  <View
+                    style={{
+                      width: 32,
+                      height: 32,
+                      borderRadius: 32 / 2,
+                      backgroundColor: emissionsCategory.color,
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Text style={{ color: colors.background, fontSize: 12 }}>
+                      {emissionsCategory.part}%
+                    </Text>
+                  </View>
+                  <View style={{ flexDirection: "column", gap: 2 }}>
+                    <Text>{t(`categories.${emissionsCategory.category}`)}</Text>
+                    {!isCompleted && (
+                      <Text
+                        variant="labelSmall"
+                        style={{ color: colors.error }}
+                      >
+                        {t(`common:toComplete`)}
+                      </Text>
+                    )}
+                  </View>
                 </View>
-                <Text>{t(`categories.${emissionsCategory.category}`)}</Text>
-              </View>
-            </DataTable.Cell>
-            <DataTable.Cell numeric>
-              {emissionsCategory.footprint} {t("common:footprintKg")}
-            </DataTable.Cell>
-          </DataTable.Row>
-        ))}
+              </DataTable.Cell>
+              <DataTable.Cell numeric>
+                {emissionsCategory.footprint} {t("common:footprintKg")}
+              </DataTable.Cell>
+            </DataTable.Row>
+          );
+        })}
     </DataTable>
   );
 };
