b1911227538efc80047a0286a3b650800e8ae57c
jessy <j.bogalho@m2mfactory.fr>
2026-08-18 08:18:52 +0200

    test(jest): remove coveragePathIgnorePatterns

--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,6 @@ export default async (): Promise<Config> => {
     transformIgnorePatterns: [
       "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
     ],
-    coveragePathIgnorePatterns: ["common"],
     moduleNameMapper: {},
   };
 
